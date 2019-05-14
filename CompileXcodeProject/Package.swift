1c1720a26d10a5260667631c8883adbb78abaa50
KeithGZQW <gzqwpay@521qw.com>
2019-05-14 10:30:24 +0800

    Swift build pass and revise compile.sh.template

--- CompileXcodeProject/Package.swift
+++ CompileXcodeProject/Package.swift
@@ -1,15 +1,18 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
   name: "CompileXcodeProject",
+  platforms: [
+    .macOS(.v10_13),
+  ],
   dependencies: [
     // Dependencies declare other packages that this package depends on.
     // .package(url: /* package url */, from: "1.0.0"),
     .package(url: "https://github.com/kareman/SwiftShell.git", from: "5.0.0"),
-    .package(url: "https://github.com/KeithPiTsui/PaversSPM.git", from: "2.0.0"),
+    .package(url: "https://github.com/KeithPiTsui/PaversSPM.git", from: "2.0.1"),
     
   ],
   targets: [
